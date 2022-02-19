dcc820636b7875f094331063204e08188c2196fb
BEU <evbolohov@yandex.ru>
2022-02-19 15:28:07 +0300

    b1_binary_search

--- Tasks/b1_binary_search.py
+++ Tasks/b1_binary_search.py
@@ -9,5 +9,32 @@ def binary_search(elem: int, arr: Sequence) -> Optional[int]:
     :param arr: array where element is to be found
     :return: Index of element if it's presented in the arr, None otherwise
     """
-    print(elem, arr)
-    return None
+
+    # middle = len(arr) // 2
+    # lb = 0 # правая граница
+    # rb = len(arr) // 2 # правая граница
+    # while True:
+    #     if elem == arr[middle]:
+    #         print(elem, arr)
+    #         return middle
+    #
+    #     if middle == 0 or middle == len(arr) - 1:
+    #         returb -1
+
+    lb = 0  # левая граница
+    rb = len(arr) - 1  # правая граница
+    middle = (lb + rb) // 2
+    while True:
+        if elem == arr[middle]:
+            if arr[middle - 1] != elem:
+                return middle
+            else:
+                middle -= 1
+        if middle == 0 or middle == len(arr) - 1:
+            return None
+        elif elem < arr[middle]:
+            rb = middle
+            middle = (lb + rb) // 2
+        elif elem > arr[middle]:
+            lb = middle + 1
+            middle = (lb + rb) // 2
